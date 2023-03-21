1207bb209eb7264d15bf6420f2ea8f82545203f2
Deepanshu Goel <72638651+DeepanshuGoel1122@users.noreply.github.com>
2023-03-21 17:46:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,10 @@
 import express from "express";
-// import mysql from "mysql";
 import mysql from "mysql2";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Lamadev123",
-//   database: "test",
-// });
 
 const db = mysql.createConnection({
     host:"localhost",
